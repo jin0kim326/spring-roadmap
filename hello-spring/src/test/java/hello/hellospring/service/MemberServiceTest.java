6c567ecf6057904803a25f08d8cec4cf4508598f
김진영C <jin0kim326@douzone.com>
2023-06-15 17:49:26 +0900

    #2 회원관리 - Service 개발 및 테스트

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,69 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+
+    }
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("jy");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    /**
+     * 예외인 경우의 테스트가 더 중요하다.
+     */
+    @Test
+    public void 회원가입_중복예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("jy");
+
+        Member member2 = new Member();
+        member2.setName("jy");
+
+        // when
+        memberService.join(member1);
+        
+        // then
+        assertThatThrownBy(() -> memberService.join(member2)).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void 회원전체찾기() throws Exception {
+    }
+
+    @Test
+    public void 회원찾기() throws Exception {
+    }
+
+}
