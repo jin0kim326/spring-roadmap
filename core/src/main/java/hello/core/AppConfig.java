459edf7345724c739cf7eebe9050c0c0b1cc3ca9
김진영C <jin0kim326@douzone.com>
2023-06-27 12:45:34 +0900

    #2-11 BeanFactory & ApplicationContext

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -37,5 +37,20 @@ public class AppConfig {
 
 /**
  * 빈 이름 : 빈이름은 기본으로 메서드 명을 사용함. @Bean(name="xxxxx")로 직접 부여가능
- * -> 빈 이름은 항상 다른 이름을 부여!!
+ *  -> 빈 이름은 항상 다른 이름을 부여!!
+ *
+ * 1.BeanFactory
+ *  -> 스프링 컨테이너의 최상위 인터페이스, 스프링빈을 관리/조회
+ *
+ * 2.ApplicationContext
+ *  -> BeanFactory 기능을 모두 상속받아 제공
+ *  -> 애플리케이션을 개발할 때는 빈을 관리/조회는 물론 수많은 "부가기능"이 필요
+ *  MessageSource - 다국어
+ *  EnvironmentCapable - 환경변수 (로컬/개발/운영)
+ *  ApplicationEventPublisher - 이벤트 발행/구독 모델을 지원
+ *  ResourceLoader - 파일/클래스패스/외부 등에서 리소스를 편리하게 조회
+ *
+ * 💡1. ApplicationContext는 BeanFactory기능을 상속받으며, 빈 관리기능 + 편리한 부가 기능 제공
+ *   2. BeanFactory를 직접사용할 일은 없음
+ *
  */
