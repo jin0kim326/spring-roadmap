17ce05ce178cfd2af39c6053f86fbbd0bc1de442
김진영C <jin0kim326@douzone.com>
2023-06-28 09:08:32 +0900

    #2-14 싱글톤 패턴 & 싱글톤 컨테이너

---
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,77 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * spring은 태생이 기업용 온라인 서비스 기술을 지원하기 위해 탄생
+ * -> 대부분의 스프링 애플리케이션은 웹 애플리케이션 (데몬,배치 등 다른것도 가능)
+ * -> 웹은 보통 여러 고객이 "동시에" 요청
+ */
+public class SingletonTest {
+
+    // 1초에 요청이 1000번 오면 1000개의 객체가 생성됨...
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        // 1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        // 2. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        // 객체생성(new)을 외부에서 못하도록 막아둠
+//        SingletonService singletonService = new SingletonService();
+
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //same == 는는비교
+        //equal
+    }
+
+    @Test
+    @DisplayName("싱글톤 컨테이너 적용")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        // 2. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+    }
+
+
+
+
+}
