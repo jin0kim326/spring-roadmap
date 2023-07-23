4cab0e238422ecad09ede9271c24e8275486f75b
김진영 <jin0kim326@gmail.com>
2023-07-23 16:16:06 +0900

    #2-25 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 해결

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,10 +3,11 @@ package hello.core.scope;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+import jakarta.inject.Provider; //추가
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -34,16 +35,15 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     @RequiredArgsConstructor
     static class ClientBean {
-        private final PrototypeBean prototypeBean;// 생성시점에 주입
-        // 싱글톤의 빈은 객체 생성시점에 이미 프로토타입빈이 주입되었기 때문에 프로토타입빈을 다시만들어지지않음
-        // =>프로토타입은 요청될때마다 새로 생성되길 바라기 때문에 사용하는것, 즉 이 예제는 잘못된 예제임
+        private final Provider<PrototypeBean> prototypeBeanProvider;
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
@@ -74,5 +74,8 @@ public class SingletonWithPrototypeTest1 {
 /**
  * 스프링은 일반적으로 싱글톤 빈을 사용하므로 싱글톤 빈이 프로토타입빈을 사용하게 됨,
  * 그런데 싱글톤 빈은 생성시점에만 의존관계 주입을 받기 때문에, 프로토타입 빈이 새로 생성은 되지만, 싱글톤빈과함께 계속유지됨.
- * 
+ *
+ * 😀 정리
+ * -> 매번 사용할 때 마다 의존관계 주입이 완료된 새로운 객체가 필요하면 사용 (실무에서는 대부분 싱글톤으로 처리)
+ * ->
  */
